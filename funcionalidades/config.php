dc4fb7175d89cecb46ba9671b8a3c03f903922f5
Bruno Barboza <brunobarbozasilva@gmail.com>
2023-07-14 02:19:17 -0300

    adicionando gitignore

--- funcionalidades/config.php
+++ funcionalidades/config.php
@@ -1,13 +1,13 @@
 <?php
-  // $conn = mysqli_connect("localhost:3307", "root", "NOVASENHA", "to_do_list") or exit("Connection Failed.");
+$host = "localhost:3307"; // colocar o seu host
+$user = "root"; // colocar o seu usuário
+$password = "NOVASENHA"; // colocar a sua senha
+$database = "to_do_list"; // colocar a sua base de dados
 
-
-
-  $mysqli = new mysqli("localhost:3307", "root", "NOVASENHA", "to_do_list");
+$mysqli = new mysqli($host, $user , $password, $database);
 
 if ($mysqli->connect_errno) {
     die('Erro ao conectar ao banco de dados: ' . $mysqli->connect_error);
 }
 ?>
-
 
